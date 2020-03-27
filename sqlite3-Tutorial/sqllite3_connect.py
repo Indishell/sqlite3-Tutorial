5fd19e842f578f7485300216a16a53bc645957be
manurag173@gmail.com <manurag173@gmail.com>
2020-03-27 22:43:15 +0530

    Sqlite3_ Connection File having insert, fetch, table creation, connection

---
+++ sqlite3-Tutorial/sqllite3_connect.py
@@ -0,0 +1,51 @@
+import sqlite3
+from sqlite3 import Error
+
+def sql_connection():
+
+    try:
+        con = sqlite3.connect('newdb.db')
+        return con
+
+    except Error:
+        print(Error)
+
+
+def sql_table(con):
+
+    cursorObj = con.cursor()
+    cursorObj.execute('CREATE TABLE EMPLOYEE(id integer PRIMARY KEY, name text, salary real, department text, designation text, hireDate text) VALUES(?, ?, ?, ?, ? , ?)', entities)
+    con.commit()
+
+
+def sql_insert(con, entities):
+    cursorObj = con.cursor()
+    
+    cursorObj.executemany('INSERT INTO EMPLOYEE(id, name, salary, department, designation, hireDate) VALUES(?, ?, ?, ?, ? , ?)', entities)
+    con.commit()
+
+
+def sql_fetch(con):
+
+    cusrorObj = con.cursor()
+    cusrorObj.execute('SELECT id, name, hireDate, from EMPLOYEE WHERE salary > 80000')
+
+    rows = cusrorObj.fetchall()
+    for row in rows:
+        print(row)
+        
+        
+def sql_update(con):
+    cusrorObj = con.cursor()
+    cusrorObj.execute('UPDATE EMPLOYEE SET name = "Sir Anurag" WHERE id = 1616')
+    con.commit()
+
+
+
+con = sql_connection()
+sql_table(con)
+entities = [ (1616, 'Anurag Mishra', 85000, 'Tech', 'Engineering Manager', '2020-05-06')  , (1617, 'Rajesh Mishra', 135000, 'EXCV', 'Senior Manager', '1988-05-06')]
+sql_insert(con, entities)
+
+sql_update(con)
+sql_fetch(con)
