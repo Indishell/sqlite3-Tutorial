31f1b8fb622edb7e15c70122f1eff01a12eae496
manurag173@gmail.com <manurag173@gmail.com>
2020-03-29 23:50:46 +0530

    I am doing it for fun

--- hi.cpp
+++ hi.cpp
@@ -1,1 +1,55 @@
-hi my name is anuraggit
+#include<bits/stdc++.h>
+using namespace std;
+
+// gcc main.cpp -o <name you want to give>
+// gcc main.cpp -o ki
+
+//  Now, Run in next command
+// Run  ki.exe    ( name you want to give.exe)
+
+int minElement(stack<int> ms)
+{
+    int temp = INT_MIN;
+
+    stack<int> ns = ms;
+
+    while (!ns.empty())
+    {
+
+        if(ns.top() > temp)
+        {
+            temp = ns.top();
+        }
+        ns.pop();        
+    }
+
+    return temp;
+}
+
+int main()
+{
+
+    stack<int> ms;
+
+    ms.push(1);
+    ms.push(-10);
+    ms.push(12);
+    ms.push(-12);
+    ms.push(13);
+    ms.push(14);
+    ms.push(18);
+    ms.push(-1);
+    
+    
+    cout<< "Min element is   " << minElement(ms) <<"\n";
+    ms.pop();
+    cout<< "Min element is   " << minElement(ms) <<"\n";
+    ms.pop();
+    cout<< "Min element is   " << minElement(ms) <<"\n";
+    ms.pop();
+    ms.pop();
+    cout<< "Min element is   " << minElement(ms) <<"\n";
+
+    
+    return 0;
+}
